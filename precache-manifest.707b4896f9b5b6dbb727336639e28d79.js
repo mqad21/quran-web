ff7d6fedc0a2b635326c61c2564ea0fd39b4202c
Muhammad Qadri <221709860@stis.ac.id>
2020-05-24 23:03:47 +0700

    Updates

---
+++ precache-manifest.707b4896f9b5b6dbb727336639e28d79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17cef77a347cb81013698ecb25ebd940",
+    "url": "/quran-web/index.html"
+  },
+  {
+    "revision": "47e660733ba9ed7790b4",
+    "url": "/quran-web/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "c33548080f34f4e4ad82",
+    "url": "/quran-web/static/css/main.7d98b71a.chunk.css"
+  },
+  {
+    "revision": "47e660733ba9ed7790b4",
+    "url": "/quran-web/static/js/2.e20f39d4.chunk.js"
+  },
+  {
+    "revision": "8a4db58a4dd46ee87637857d04e63845",
+    "url": "/quran-web/static/js/2.e20f39d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33548080f34f4e4ad82",
+    "url": "/quran-web/static/js/main.06d744f4.chunk.js"
+  },
+  {
+    "revision": "e8f1b99e2acf5c61ef02",
+    "url": "/quran-web/static/js/runtime-main.aed27342.js"
+  }
+]);
